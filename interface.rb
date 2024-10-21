9c7da763a21515731ee538650a18f698e38d769a
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-21 17:15:49 -0300

    refactor com explicações

--- interface.rb
+++ interface.rb
@@ -1,8 +1,8 @@
 fruits = {
-"banana" => {price: 1.2, availability: 5},
-"kiwi" => {price: 3.0, availability: 6},
-"manga" => {price: 5.0, availability: 2},
-"abacaxi" => {price: 10.01, availability: 8},
+  "banana" => {price: 1.2, availability: 5},
+  "kiwi" => {price: 3.0, availability: 6},
+  "manga" => {price: 5.0, availability: 2},
+  "abacaxi" => {price: 10.01, availability: 8},
 }
 
 # Boas vindas
@@ -16,12 +16,13 @@ cart = []
 
 # LOOP
 loop do
-  # Mostrar os produtos com o respectivo preço
+  # Mostrar os produtos com o respectivo preço e disponibilidade
+  # info é hash do tipo {price: 1.2, availability: 5}
   fruits.each do |fruit, info|
     puts "#{fruit} a R$ #{info[:price]} Disponível #{info[:availability]}"
   end
   # Perguntar qual produto ele quer comprar
-  puts "Escolha seu produto  "
+  puts "Escolha seu produto ou quit para encerrar:"
   product = gets.chomp
 
   # o produto for `quit` sai do loop
@@ -32,34 +33,57 @@ loop do
     # Perguntar a quantidade
     puts "Quantidade?"
     quantity = gets.chomp.to_i
+
+
+    # Determinar a disponibilidade:
+    # product == "abacaxi"
+    # fruits[product] == {price: 10.01, availability: 8}
+    # fruits[product][:availability] == 8
     availability = fruits[product][:availability]
+
+    # Verificar se a quantidade digitada é menor ou igual a que está disponível
     if quantity <= availability
-      cart << {product: product, quantity: quantity} # Adiciona ao cart o produto
+      # Sim, temos a quantidade pedida
+      cart << {product: product, quantity: quantity} # Adiciona ao cart o produto e quantidade
+      # Remover da disponibilidade o que foi pedido
       fruits[product][:availability] -= quantity
     else
+      # Não temos a quantidade solicitada
       puts "Estoque indisponível. Só temos #{availability} de #{product}"
     end
   else
-    # Mensagem de erro que o produto não existe
-    puts "Produto não disponivel, procure outro ou quit"
+    puts "Produto não disponivel, procure outro ou quit" # Mensagem de erro que o produto não existe
   end
 end# FIM DO LOOP
 
-# [{:product=>"kiwi", :quantity=>3}, {:product=>"banana", :quantity=>2}, {:product=>"abacaxi", :quantity=>1}]
+# Exemplo de um cart:
+# cart = [
+#         {:product=>"kiwi", :quantity=>3},
+#         {:product=>"banana", :quantity=>2},
+#         {:product=>"abacaxi", :quantity=>1}
+#        ]
 
 # Calcular o total dos produtos
 total = 0
+
+puts "--------------------"
 cart.each do |info|
-  # Calcular o subtotal por produto X quantidade
-  product = info[:product]
-  quantity = info[:quantity]
+  # info é um hash do tipo {:product=>"kiwi", :quantity=>3}
+  product = info[:product] # => "kiwi"
+  quantity = info[:quantity] # => 3
+
+  # fruits["kiwi"] == {price: 3.0, availability: 6}
+  # fruits["kiwi"][:price] == 3.0
   price = fruits[product][:price]
+
   subtotal = price * quantity
+
   puts "Product: #{product} #{quantity} x #{price} = #{subtotal.round(2)}"
+
   total += subtotal
 end
 
 # Mostrar o total
-puts "--------------------"
+
 puts " Total R$ #{total}"
 puts "--------------------"
