1c74774975364241b4d6a5e28a354a94f90d1f1d
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-21 15:00:45 -0300

    pseudocode

---
+++ interface.rb
@@ -0,0 +1,23 @@
+# Boas vindas
+puts "--------------------"
+puts " Welcome to Instacart"
+puts "--------------------"
+
+# Mostrar os produtos com o respectivo preço
+
+# LOOP
+
+  # Perguntar qual produto ele quer comprar
+
+  # Verificar se o produto existe, ou dar uma mensagem de erro
+
+  # o produto for `quit` sai do loop
+
+# FIM DO LOOP
+
+# Calcular o total dos produtos
+
+# Mostrar o total
+puts "--------------------"
+puts " Total R$ ????"
+puts "--------------------"
